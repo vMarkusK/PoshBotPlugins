c6872a82836a12c9e4f293f5f8a71f4d334173f5
Markus Kraus <markus.kraus@gmail.com>
2017-11-25 12:50:04 +0100

    seperate Build Script

--- appveyor/deploy.ps1
+++ appveyor/deploy.ps1
@@ -1,10 +1,17 @@
 Write-Host "Running AppVeyor deploy script" -ForegroundColor Yellow
+
+# Update manifest Version
 Write-Host "Creating new module manifest"
-$ModuleManifestPath = Join-Path -path "$($env:APPVEYOR_BUILD_FOLDER)\poshbot.veeam\" -ChildPath ("poshbot.veeam.psd1")
-$ModuleManifest = Get-Content $ModuleManifestPath -Raw
+$ModuleManifestPath = Join-Path -path "$($env:APPVEYOR_BUILD_FOLDER)\poshbot.veeam\" -ChildPath ("$env:ModuleName"+'.psd1')
+$ModuleManifest     = Get-Content $ModuleManifestPath -Raw
 [regex]::replace($ModuleManifest,'(ModuleVersion = )(.*)',"`$1'$env:APPVEYOR_BUILD_VERSION'") | Out-File -LiteralPath $ModuleManifestPath
-if ($env:APPVEYOR_REPO_BRANCH -notmatch 'master') {
-    Write-Host "Finished testing of branch $env:APPVEYOR_REPO_BRANCH - Exiting"
-    exit
+
+# Pubklish to PS Gallery
+if ($env:APPVEYOR_REPO_BRANCH -notmatch 'master')
+{
+    Write-Host "Finished testing of branch: $env:APPVEYOR_REPO_BRANCH - Exiting"
+    exit;
 }
+
+Write-Host 'Publishing module to Powershell Gallery'
 #Update-PowerShellGallery -Path "$($env:APPVEYOR_BUILD_FOLDER)\poshbot.veeam\" -ApiKey $env:NuGetApiKey
