79ad126f2a84f4f71a112f6584af9c2df3804f0d
Markus Kraus <markus.kraus@gmail.com>
2017-11-26 00:30:21 +0100

    New Job Session Report Engine

--- poshbot.veeam/poshbot.veeam.psm1
+++ poshbot.veeam/poshbot.veeam.psm1
@@ -2,6 +2,247 @@
 Add-PSSnapin VeeamPSSnapIn
 #endregion
 
+#region: Private Functions
+## Source: https://github.com/tdewin/randomsamples/tree/master/powershell-veeamallstat
+class VeeamAllStatJobSessionVM {
+    $name
+    $status;
+    $starttime;
+    $endtime;
+    $size;
+    $read;
+    $transferred;
+    $duration;
+    $details;
+    VeeamAllStatJobSessionVM() {}
+    VeeamAllStatJobSessionVM($name,$status,$starttime,$endtime,$size,$read,$transferred,$duration,$details) {
+        $this.name = $name
+        $this.status = $status;
+        $this.starttime = $starttime;
+        $this.endtime = $endtime;
+        $this.size = $size;
+        $this.read = $read;
+        $this.transferred = $transferred;
+        $this.duration  = $duration;
+        $this.details = $details
+    }
+}
+
+class VeeamAllStatJobSession {
+    $name = "<no name>"
+    $type = "<no type>";
+    $description = "";
+    $status = "<not run>";
+    [System.DateTime]$creationTime='1970-01-01 00:00:00';
+    [System.DateTime]$endTime='1970-01-01 00:00:00';
+    [System.TimeSpan]$duration
+    $processedObjects=0;
+    $totalObjects=0
+    $totalSize=0
+    $backupSize=0
+    $dataRead=0
+    $transferredSize=0
+    $dedupe=0
+    $compression=0
+    $details=""
+    $vmstotal=0
+    $vmssuccess=0
+    $vmswarning=0
+    $vmserror=0
+    $allerrors=@{}
+    [bool]$hasRan=$true
+    [VeeamAllStatJobSessionVM[]]$vmsessions= @()
+    VeeamAllStatJobSession() { }
+    VeeamAllStatJobSession($name,$type,$description) {
+        $this.name = $name
+        $this.type = $type;
+        $this.description =$description;
+    }
+    VeeamAllStatJobSession($name,$type,$description,$status,$creationTime,$endtime,$processedObjects,$totalObjects,$totalSize,$backupSize,$dataRead,$transferredSize,$dedupe,$compression,$details) {
+        $this.name = $name
+        $this.type = $type;
+        $this.description =$description;
+        $this.status = $status;
+        $this.creationTime=$creationTime;
+        $this.endTime=$endTime;
+        $this.processedObjects=$processedObjects;
+        $this.totalObjects=$totalObjects
+        $this.totalSize=$totalSize
+        $this.backupSize=$backupSize
+        $this.dataRead=$dataRead
+        $this.transferredSize=$transferredSize
+        $this.dedupe=$dedupe
+        $this.compression=$compression
+        $this.details=$details
+        $this.duration=$endTime-$creationTime
+    }
+}
+
+class VeeamAllStatJobMain {
+    $versionVeeam
+    $server
+    $serverString
+    [VeeamAllStatJobSession[]]$jobsessions
+    VeeamAllStatJobMain () {
+        $this.jobsessions = @()
+    }
+}
+
+
+
+function Get-VeeamAllStatJobSessionVMs {
+    param(
+        $session,
+        [VeeamAllStatJobSession]$statjobsession
+    )
+
+    $tasks = $session.GetTaskSessions()
+
+    foreach($task in $tasks) {
+        $s = $task.status
+        $vm = [VeeamAllStatJobSessionVM]::new(
+            $task.Name,
+            $s,
+            $task.Progress.StartTime,
+            $task.Progress.StopTime,
+            $task.Progress.ProcessedSize,
+            $task.Progress.ReadSize,
+            $task.Progress.TransferedSize,
+            $task.Progress.Duration,
+            $task.GetDetails())
+
+        if ($s -ieq "success") {
+            $statjobsession.vmssuccess += 1
+        } elseif ($s -ieq "warning" -or $s -ieq "pending" -or $s -ieq "none") {
+            $statjobsession.vmswarning +=1
+        } else {
+            $statjobsession.vmserror += 1
+        }
+        if ($vm.details -ne "") {
+            $statjobsession.allerrors[$task.Name]=$vm.details
+        }
+        $statjobsession.vmsessions += $vm
+        $statjobsession.vmstotal+=1
+    }
+}
+
+function Get-VeeamAllStatJobSession {
+    param(
+        $job,
+        $session
+    )
+    $statjob = [VeeamAllStatJobSession]::new(
+        $job.Name,
+        $session.JobType,
+        $job.Description,
+        $session.Result,
+        $session.CreationTime,
+        $session.EndTime,
+        $session.Progress.ProcessedObjects,
+        $session.Progress.TotalObjects,
+        $session.Progress.TotalSize,
+        $session.BackupStats.BackupSize,
+        $session.Progress.ReadSize,
+        $Session.Progress.TransferedSize,
+        $session.BackupStats.GetDedupeX(),
+        $session.BackupStats.GetCompressX(),
+        $session.GetDetails()
+    )
+    Get-VeeamAllStatJobSessionVMs -session $session -statjobsession $statjob
+
+    if ($session.Result -eq "None" -and $session.JobType -eq "BackupSync") {
+        if($session.State -eq "Idle" -and $statjob.vmserror -eq 0 -and $statjob.vmswarning -eq 0 -and $statjob.allerrors.count -eq 0 -and $statjob.details -eq ""  -and $session.EndTime -gt $session.CreationTime ) {
+            if ($session.Progress.Percents -eq 100) {
+                $statjob.Status="Success"
+            }
+        }
+    }
+
+    return $statjob
+}
+
+function Get-VeeamAllStatJobSessions {
+    param(
+        [VeeamAllStatJobMain]$JobMain
+    )
+
+    $allsessions = Get-VBRBackupSession
+    $allorderdedsess = $allsessions | Sort-Object -Property CreationTimeUTC -Descending
+    $jobs = get-vbrjob
+
+    foreach ($Job in $Jobs) {
+        $lastsession = $allorderdedsess | ? { $_.jobid -eq $Job.id } | select -First 1
+        if ($lastsession -ne $null) {
+           $JobMain.jobsessions += Get-VeeamAllStatJobSession -job $job -session $lastsession
+        } else {
+           $s = [VeeamAllStatJobSession]::new($job.Name,$job.type,$job.description)
+           $s.hasRan = $false
+           $JobMain.jobsessions += $s
+        }
+    }
+
+}
+
+function Get-VeeamAllStatServerVersion {
+    param(
+        [VeeamAllStatJobMain]$JobMain
+    )
+    $versionstring = "Unknown Version"
+
+    $pssversion = (Get-PSSnapin VeeamPSSnapin -ErrorAction SilentlyContinue)
+    if ($pssversion -ne $null) {
+        $versionstring = ("{0}.{1}" -f $pssversion.Version.Major,$pssversion.Version.Minor)
+    }
+
+    $corePath = Get-ItemProperty -Path "HKLM:\Software\Veeam\Veeam Backup and Replication\" -Name "CorePath" -ErrorAction SilentlyContinue
+    if ($corePath -ne $null) {
+        $depDLLPath = Join-Path -Path $corePath.CorePath -ChildPath "Packages\VeeamDeploymentDll.dll" -Resolve -ErrorAction SilentlyContinue
+        if ($depDLLPath -ne $null -and (Test-Path -Path $depDLLPath)) {
+            $file = Get-Item -Path $depDLLPath -ErrorAction SilentlyContinue
+            if ($file -ne $null) {
+                $versionstring = $file.VersionInfo.ProductVersion
+            }
+        }
+    }
+
+    $servsession = Get-VBRServerSession
+    $JobMain.versionVeeam = $versionstring
+    $JobMain.server = $servsession.server
+    $JobMain.serverString = ("Server {0} : Veeam Backup & Replication {1}" -f $servsession.server,$versionstring)
+}
+
+function Get-VeeamAllStat {
+    $report = [VeeamAllStatJobMain]::new()
+
+    Get-VeeamAllStatServerVersion -JobMain $report
+    Get-VeeamAllStatJobSessions -JobMain $report
+    return $report
+}
+
+function Get-HumanDataSize {
+ param([double]$numc)
+ $num = $numc+0
+ $trailing= "","K","M","G","T","P","E"
+ $i=0
+ while($num -gt 1024 -and $i -lt 6) {
+  $num= $num/1024
+  $i++
+ }
+ return ("{0:f1} {1}B" -f $num,$trailing[$i])
+}
+
+function Get-HumanDate {
+    param([DateTime]$t)
+    return $t.toString("yyyy-MM-dd HH:mm:ss")
+}
+
+function Get-HumanDuration {
+    param([System.TimeSpan]$d)
+    return ("{0:D2}:{1:D2}:{2:D2}" -f ($d.Hours+($d.Days*24)),$d.Minutes,$d.Seconds)
+}
+#endregion
+
+#region: Public Functions
 $CommandsToExport = @()
 
 function Get-VeeamRepositories {
@@ -123,8 +364,6 @@ function Get-VeeamSessions {
         .EXAMPLE
         !Get-VeeamSessions
         .EXAMPLE
-        !Get-VeeamSessions --reportMode Monthly
-        .EXAMPLE
         !Get-VeeamSessions --brhost <your BR Host>
         .EXAMPLE
         !Sessions
@@ -139,88 +378,35 @@ function Get-VeeamSessions {
         [cmdletbinding()]
         param(
                 [Parameter(Position=0, Mandatory=$false)]
-                        [string] $BRHost = "localhost",
-                [Parameter(Position=1, Mandatory=$false)]
-                        $reportMode = "24" # Weekly, Monthly as String or Hour as Integer
-
+                        [string] $BRHost = "localhost"
         )
-
-        #region: Convert mode (timeframe) to hours
-        If ($reportMode -eq "Monthly") {
-                $HourstoCheck = 720
-        } Elseif ($reportMode -eq "Weekly") {
-                $HourstoCheck = 168
-        } Else {
-                $HourstoCheck = $reportMode
-        }
-        #endregion
 
         #region: Start BRHost Connection
         Connect-VBRServer -Server $BRHost
         #endregion
 
         #region: Collect Sessions
-        $allSesh = Get-VBRBackupSession         # Get all Sessions (Backup/BackupCopy/Replica)
-        $seshListBk = @($allSesh | Where-Object{($_.CreationTime -ge (Get-Date).AddHours(-$HourstoCheck)) -and $_.JobType -eq "Backup"})           # Gather all Backup sessions within timeframe
-        $seshListBkc = @($allSesh | Where-Object{($_.CreationTime -ge (Get-Date).AddHours(-$HourstoCheck)) -and $_.JobType -eq "BackupSync"})      # Gather all BackupCopy sessions within timeframe
-        $seshListRepl = @($allSesh | Where-Object{($_.CreationTime -ge (Get-Date).AddHours(-$HourstoCheck)) -and $_.JobType -eq "Replica"})        # Gather all Replication sessions within timeframe
-        #endregion
-
-        #region: Get Backup session informations
-        $TotalBackupTransfer = 0
-        $TotalBackupRead = 0
-        $seshListBk | ForEach-Object{$TotalBackupTransfer += $([Math]::Round([Decimal]$_.Progress.TransferedSize/1GB, 0))}
-        $seshListBk | ForEach-Object{$TotalBackupRead += $([Math]::Round([Decimal]$_.Progress.ReadSize/1GB, 0))}
-        #endregion
-
-        #region: Preparing Backup Session Reports
-        $successSessionsBk = @($seshListBk | Where-Object{$_.Result -eq "Success"})
-        $warningSessionsBk = @($seshListBk | Where-Object{$_.Result -eq "Warning"})
-        $failsSessionsBk = @($seshListBk | Where-Object{$_.Result -eq "Failed"})
-        $runningSessionsBk = @($allSesh | Where-Object{$_.State -eq "Working" -and $_.JobType -eq "Backup"})
-        $failedSessionsBk = @($seshListBk | Where-Object{($_.Result -eq "Failed") -and ($_.WillBeRetried -ne "True")})
-        #endregion
-
-        #region:  Preparing Backup Copy Session Reports
-        $successSessionsBkC = @($seshListBkC | Where-Object{$_.Result -eq "Success"})
-        $warningSessionsBkC = @($seshListBkC | Where-Object{$_.Result -eq "Warning"})
-        $failsSessionsBkC = @($seshListBkC | Where-Object{$_.Result -eq "Failed"})
-        $runningSessionsBkC = @($allSesh | Where-Object{$_.State -eq "Working" -and $_.JobType -eq "BackupSync"})
-        $IdleSessionsBkC = @($allSesh | Where-Object{$_.State -eq "Idle" -and $_.JobType -eq "BackupSync"})
-        $failedSessionsBkC = @($seshListBkC | Where-Object{($_.Result -eq "Failed") -and ($_.WillBeRetried -ne "True")})
-        #endregion
-
-        #region: Preparing Replicatiom Session Reports
-        $successSessionsRepl = @($seshListRepl | Where-Object{$_.Result -eq "Success"})
-        $warningSessionsRepl = @($seshListRepl | Where-Object{$_.Result -eq "Warning"})
-        $failsSessionsRepl = @($seshListRepl | Where-Object{$_.Result -eq "Failed"})
-        $runningSessionsRepl = @($allSesh | Where-Object{$_.State -eq "Working" -and $_.JobType -eq "Replica"})
-        $failedSessionsRepl = @($seshListRepl | Where-Object{($_.Result -eq "Failed") -and ($_.WillBeRetried -ne "True")})
+        $Stats = (Get-VeeamAllStat).jobsessions
         #endregion
 
         #region: Build Report
-        $SessionObject = [PSCustomObject] @{
-                "Successful Backups"  = $successSessionsBk.Count
-                "Warning Backups" = $warningSessionsBk.Count
-                "Failes Backups" = $failsSessionsBk.Count
-                "Failed Backups" = $failedSessionsBk.Count
-                "Running Backups" = $runningSessionsBk.Count
-                "Warning BackupCopys" = $warningSessionsBkC.Count
-                "Failes BackupCopys" = $failsSessionsBkC.Count
-                "Failed BackupCopys" = $failedSessionsBkC.Count
-                "Running BackupCopys" = $runningSessionsBkC.Count
-                "Idle BackupCopys" = $IdleSessionsBkC.Count
-                "Successful Replications" = $successSessionsRepl.Count
-                "Warning Replications" = $warningSessionsRepl.Count
-                "Failes Replications" = $failsSessionsRepl.Count
-                "Failed Replications" = $failedSessionsRepl.Count
-                "Running Replications" = $RunningSessionsRepl.Count
-                "Total Backup Transfer" = $TotalBackupTransfer
-                "Total Backup Read" = $TotalBackupRead
-        }
-        $SessionResport += $SessionObject
+        forEach ($Stat in $Stats) {
 
-        New-PoshBotCardResponse -Title "Session Stats for the last $HourstoCheck h:" -Text ($SessionResport | Format-List -Property * | Out-String)
+                                $r = [pscustomobject]@{
+                                        JobName = $Stat.name
+                                        JobType = $Stat.type
+                                        HasRan = $Stat.hasRan
+                                        JobStatus = $Stat.status
+                                        CreationTime  = $Stat.creationTime
+                                        EndTime  = $Stat.endTime
+                                        VMsTotal = $Stat.vmstotal
+                                        VMsSuccess = $Stat.vmssuccess
+                                        VMsWarning = $Stat.vmswarning
+                                        VMsError  = $Stat.vmserror
+                                    }
+
+                                New-PoshBotCardResponse -Title "Job '$($Stat.name)' Stats:" -Text ($r | Format-List -Property * | Out-String)
+                                }
         #endregion
 }
 $CommandsToExport += 'Get-VeeamSessions'
@@ -263,9 +449,6 @@ function Get-VeeamJobs {
                 $r = [pscustomobject]@{
                         Name = $Job.Name
                         JobType = $Job.JobType
-                        IsRunning = $Job.IsRunning
-                        IsScheduleEnabled = $Job.IsScheduleEnabled
-                        NextRun = $Job.ScheduleOptions.NextRun
                     }
 
                 New-PoshBotCardResponse -Title "$($Job.'Name'):" -Text ($r | Format-List -Property * | Out-String)
@@ -275,3 +458,4 @@ function Get-VeeamJobs {
 $CommandsToExport += 'Get-VeeamJobs'
 
 Export-ModuleMember -Function $CommandsToExport
+#endregsion
